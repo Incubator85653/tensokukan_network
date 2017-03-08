76e3ccadf7bde05d9f11615ad3d595249307e643
Incubator85653 <2bemperor47@gmail.com>
2017-03-08 22:15:31 +0800

    Added UTF-8 support
    Keep an old lib file for old versions.
    These versions have no reason unable to output UTF-8.
    
    "stdout_to_cp932_converter.rb" will be finally remove once Tensokukan
    2017 was released.

--- lib/tenco_reporter/stdout_to_cp65001_converter.rb
+++ lib/tenco_reporter/stdout_to_cp65001_converter.rb
@@ -2,13 +2,7 @@
 
 require 'nkf'
 
-# This file is not going to convert stdout to cp932.
-# Don't get confused with the file name...
-
-# Tensokukan 2017 added Unicode support.
-# This lib make old versions able to output cp65001.
-
-# convert $stdout to cp65001...
+# convert $stdout to cp65001
 if !defined?($stdout._write)
   class << $stdout
     alias :_write :write
