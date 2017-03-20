2201de971bfd8e6b71542eebf4cd98c8c2a18adc
Incubator85653 <2bemperor47@gmail.com>
2017-03-20 12:57:38 +0800

    Update delete source script

--- build_tools/delete_source.cmd
+++ build_tools/delete_source.cmd
@@ -8,16 +8,21 @@
 @set config_default=config_default.yaml
 @set env=env.yaml
 @set variables=variables.yaml
+@set string=string.yaml
 
 @REM folders
 @set lib=lib
+@set bin=bin
 
 %del% %main%
 %del% %config%
 %del% %config_default%
 %del% %env%
 %del% %variables%
+%del% %string%
+
 
 %rd% %lib%
+%rd% %bin%
 
 pause
